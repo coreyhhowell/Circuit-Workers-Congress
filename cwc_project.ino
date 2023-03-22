82a7f0cab526ea22ec94dfcda601ad5197993dc2
Corey Howell <103910362+coreyhhowell@users.noreply.github.com>
2023-03-22 13:45:06 -0400

    Update to CWC
    Added joystick for testing, edited code for semantics, painless mesh + dependencies

--- cwc_project.ino
+++ cwc_project.ino
@@ -1,3 +1,17 @@
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
+
+#include <TScheduler.hpp>
+#include <TSchedulerDeclarations.hpp>
+#include <TaskScheduler.h>
+#include <TaskSchedulerDeclarations.h>
+#include <TaskSchedulerSleepMethods.h>
+
+#include <painlessMesh.h>
+#define   MESH_PREFIX     "whateverYouLike"
+#define   MESH_PASSWORD   "somethingSneaky"
+#define   MESH_PORT       5555
+
 #include <HCSR04.h>
 const int triggerPin = 33;
 const int echoPin = 34; 
@@ -18,35 +32,46 @@ const int AIN2 = 16;
 // The speed of the motors:
 const int motorSpeed = 128; // Half speed (255 / 2).
 
-const int testLED = 35;
+//Demo Joystick
+const int joystick_Switch_pin = 3; 
+const int joystick_X_pin = 5;
+const int joystick_Y_pin = 4;
 
 
 void setup() {
+  
   Serial.begin(9600);
-  pinMode(testLED, OUTPUT);
   HCSR04.begin(triggerPin, echoPin);
+  pixels.setPixelColor(0, 130);
+  pixels.show();   
   delay(1000);
+  
   Serial.println("Hello World");
-  
-  pinMode(LED_PIN, OUTPUT);
-  
-  
-    pinMode(AIN1, OUTPUT);
+  pixels.setPixelColor(255, 255);
+  pixels.show();   
+  Serial.println("Setting pin modes");
+  pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
   pinMode(BIN1, OUTPUT);
   pinMode(BIN2, OUTPUT);
   pinMode(nSleep, OUTPUT); //nSleep Mode = On when logic HIGH, logic LOW to sleep
   pinMode(FLT, INPUT);
-  digitalWrite(nSleep, HIGH);
 
   digitalWrite(AIN1, LOW);
   digitalWrite(AIN2, LOW);
-   digitalWrite(BIN1, LOW);
+  digitalWrite(BIN1, LOW);
   digitalWrite(BIN2, LOW);
   
+  pinMode(joystick_Switch_pin, INPUT);
+  pinMode(joystick_X_pin, INPUT);
+  pinMode(joystick_Y_pin, INPUT);
+  
+  Serial.println("Almost done");
   pixels.setBrightness(10);
   pixels.begin(); 
   Serial.println("Ready!");
+  pixels.setPixelColor(0, 255);
+  pixels.show();   
 }
 // Input a value 0 to 255 to get a color value.
 uint32_t Wheel(byte WheelPos)
@@ -73,40 +98,61 @@ uint32_t Wheel(byte WheelPos)
 //Counter to run from 0-255 to cycle the colors of the rainbow.
 int colorCount = 0;
 
-void loop() {
-  
-   double* distances = HCSR04.measureDistanceCm();
+void motorsForward() {
 
-int testDistance = HCSR04.measureDistanceCm();
-    if (testDistance < 3.0) {
-      digitalWrite(testLED, HIGH);
+}
+
+void loop() {
+  double* distances = HCSR04.measureDistanceCm();
+
+  //Test distance, gets pulled from distances array
+  //Determine proxemics in conditionals below
+  int testDistance = distances[0];  
+    if (testDistance < 10 {
+      Serial.println("Proxemic 1");
+      pixels.setPixelColor(0, 20);
+      pixels.show();  
+    } else if (#CONDITION2) {
+      Serial.println("Proxemic 2");
+      pixels.setPixelColor(0, 100);
+      pixels.show();  
+    } else if (#CONDITION3) {
+      Serial.println("Proxemic 3");
+      pixels.setPixelColor(0, 200);
+      pixels.show();        
     }
-  
-  pixels.setPixelColor(0, Wheel(colorCount++));
+
+  //Running pixel setting
+  pixels.setPixelColor(0, 170);
   pixels.show();   
-    if (colorCount > 255)
-        colorCount = 0;
+    
   delay(1000);
-  Serial.print("1: ");
+  
+  //Print distances array, label correct unit (centimeter)
   Serial.print(distances[0]);
   Serial.println(" cm");
-  
   Serial.println("---");
-  delay(1000);
+  delay(500);
 
-  digitalWrite(LED_PIN, HIGH);
-  digitalWrite(AIN1, HIGH);
-  digitalWrite(BIN1, HIGH);
-  //digitalWrite(AIN2, LOW);
-  analogWrite(AIN1, motorSpeed);
-  analogWrite(BIN1, motorSpeed);  
-  Serial.println("LED on");
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  digitalWrite(AIN1, LOW);
-  digitalWrite(BIN1, LOW);
-  analogWrite(AIN1, 0);
-  analogWrite(BIN1, 0);
-  Serial.println("LED off");
-  delay(1000);
+  //For testing
+  //read values from joystick, assign to temp int, print to serial
+  int joystick_X = analogRead(joystick_X_pin);
+  int joystick_Y = analogRead(joystick_Y_pin);
+  Serial.print("Joystick X: ");
+  Serial.println(joystick_X);
+  Serial.print("Joystick Y: ");
+  Serial.println(joystick_Y);
+
+  // digitalWrite(AIN1, HIGH);
+  // digitalWrite(BIN1, HIGH);
+  // digitalWrite(AIN2, LOW);
+  // analogWrite(AIN1, motorSpeed);
+  // analogWrite(BIN1, motorSpeed);  
+  // delay(1000);
+  // digitalWrite(AIN1, LOW);
+  // digitalWrite(BIN1, LOW);
+  // analogWrite(AIN1, 0);
+  // analogWrite(BIN1, 0);
+  // delay(1000);
+  return;
 }
